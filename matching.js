c162dbe09e33f93a7303f5531b1e8d8d4b504c2b
celic <cceli@codequarry.net>
2014-11-15 00:22:52 -0500

    Adds initial js

--- matching.js
+++ matching.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
+	$box1 = $('#box1');
+	$box2 = $('#box2');
+	$box3 = $('#box3');
+	$box4 = $('#box4');
+	$box5 = $('#box5');
+	$box6 = $('#box6');
+	$box7 = $('#box7');
+	$box8 = $('#box8');
+	$box9 = $('#box9');
+	$box10 = $('#box10');
+	$box11 = $('#box11');
+	$box12 = $('#box12');
+	$box13 = $('#box13');
+	$box14 = $('#box14');
+	$box15 = $('#box15');
+	$box16 = $('#box16');
 
+	var clicked = 0;
+	var matches = 0;
 
-}
+	$(document).on('click', '.box', function (sel){
+
+		alert(sel.target.id);
+	});
+
+});
