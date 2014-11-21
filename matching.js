f67efe108cf7157b1ac312922ecca46892624832
celic <cceli@codequarry.net>
2014-11-21 12:27:21 -0500

    Shuffle options

--- matching.js
+++ matching.js
@@ -22,22 +22,31 @@ $(document).ready(function(){
 	var first_clicked = 0;
 	var second_clicked = 0;
 
+	var pictures = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+	var descriptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 12, 13, 14, 15];
+	var long_descriptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, ,13, ,14, 15];
+
+	shuffle();
+
 	$(document).on('click', '.box', function (sel){
 
 		var box_id = "#" + sel.target.id;
 		$(box_id).fadeTo(500, 0, function() {
+			
 			if(clicked == 0){
 
-			// Just let it sit until the next one is clicked
-			first_clicked = sel.target.id;
-			clicked = 1;
-			
+				// Just let it sit until the next one is clicked
+				first_clicked = sel.target.id;
+				clicked = 1;
+				
 			}else{
 
 				second_clicked = sel.target.id;
 				clicked = 0;
 
 				// Check match
+
+				// If match keep faded
 
 				// If no match fade both back in
 				var box_id1 = "#" + first_clicked;
@@ -48,4 +57,18 @@ $(document).ready(function(){
 			}
 		});
 	});
+
+	function shuffle(){
+
+		// Randomize an array of 15 elements
+		var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+		for(var i = 0; i < 15; i++){
+		
+			var index = Math.floor(Math.random() * 15);
+		
+			var tmp = arr[i];
+			arr[i] = arr[index];
+			arr[index] = tmp;
+		}
+	};
 });
