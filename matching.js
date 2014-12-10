ad05818c2f03a9e875a400bf8c41b6b8275f192d
celic <cceli@codequarry.net>
2014-12-10 18:24:30 -0500

    Finishes images and other changes

--- matching.js
+++ matching.js
@@ -31,16 +31,18 @@ $(document).ready(function(){
 	var game_questions = [];
 	var game_board = [];
 
+	var match_id = 0;
+
 	var content = [
-					{"picture": "images/greenflash.png", "description": 110, "long_description": "A green flash occurs just before sunrise or after sunset, and will only last a couple seconds. These green rays of light are caused by the atmosphere's ability to separate the light of the sun into it's different components."},
-					{"picture": "images/kuiperbelt.png", "description": 111, "long_description": "The Kuiper Belt is very similar to the astroid belt, but it is located beyond the orbit of Neptune. Objects in the Kuiper Belt are often icy masses of methane ammonia or water. Pluto is perhaps the most famous 'Kuiper Belt Object.'"},
-					{"picture": "images/mooncreation.png", "description": 112, "long_description": "The image describes the most prominent theory of the moon's creation. Scientists believe that a Mars-sized object collided with the Earth creating massive amounts of debris. This debris collected in an orbit around the Earth eventually compacting enough to form the Moon."},
-					{"picture": "images/moonreverse.png", "description": 113, "long_description": "The Moon has what is called 'synchronous rotation' with the Earth meaning that it orbits the Earth at the same rate it rotates around its own axis. Because of this from Earth, we only see one side of the moon. Depicted is the far side of the moon."},
-					{"picture": "images/reflectingtelescope.png", "description": 114, "long_description": "Most astronomical telescopes nowadays are reflecting telescopes because of how compact yet effective they are compared to refracting telescopes. Reflectors use concave mirrors to act as a lens reflecting the image through the same tube, extending the focal length while using less material."},
-					{"picture": "images/refractingtelescope.png", "description": 115, "long_description": "Refracting telescopes are what most people imagine a telescope to be. They are often long, skinny tubes with an objective lens on the wide end and an eyepeice on the opposite end. Because these telescopes are long and straight, they are very difficult to scale up to larger sizes."},
-					{"picture": 6, "description": 116, "long_description": 6},
-					{"picture": 7, "description": 117, "long_description": 7},
-					{"picture": 8, "description": 118, "long_description": 8},
+					{"picture": "images/greenflash.png", 			"description": "images/names/greenflash.png", 			"long_description": "A green flash occurs just before sunrise or after sunset, and will only last a couple seconds. These green rays of light are caused by the atmosphere's ability to separate the light of the sun into it's different components."},
+					{"picture": "images/kuiperbelt.png", 			"description": "images/names/kuiperbelt.png", 			"long_description": "The Kuiper Belt is very similar to the astroid belt, but it is located beyond the orbit of Neptune. Objects in the Kuiper Belt are often icy masses of methane ammonia or water. Pluto is perhaps the most famous 'Kuiper Belt Object.'"},
+					{"picture": "images/mooncreation.png", 			"description": "images/names/mooncreation.png", 		"long_description": "The image describes the most prominent theory of the moon's creation. Scientists believe that a Mars-sized object collided with the Earth creating massive amounts of debris. This debris collected in an orbit around the Earth eventually compacting enough to form the Moon."},
+					{"picture": "images/moonreverse.png", 			"description": "images/names/moonreverse.png", 			"long_description": "The Moon has what is called 'synchronous rotation' with the Earth meaning that it orbits the Earth at the same rate it rotates around its own axis. Because of this from Earth, we only see one side of the moon. Depicted is the far side of the moon."},
+					{"picture": "images/reflectingtelescope.png", 	"description": "images/names/reflectingtelescope.png", 	"long_description": "Most astronomical telescopes nowadays are reflecting telescopes because of how compact yet effective they are compared to refracting telescopes. Reflectors use concave mirrors to act as a lens reflecting the image through the same tube, extending the focal length while using less material."},
+					{"picture": "images/refractingtelescope.png", 	"description": "images/names/refractingtelescope.png", 	"long_description": "Refracting telescopes are what most people imagine a telescope to be. They are often long, skinny tubes with an objective lens on the wide end and an eyepeice on the opposite end. Because these telescopes are long and straight, they are very difficult to scale up to larger sizes."},
+					{"picture": "images/m82starforming.png", 		"description": "images/names/starforming.png", 			"long_description": "Shown is a starburst galaxy. Stars in these galaxies are created from cold gas that forms into large clouds. When this gas condenses a star is born."},
+					{"picture": "images/andromedagalaxy.png", 		"description": "images/names/andromeda.png", 			"long_description": "The Andromeda galaxy is a classic spiral galaxy. In this ultraviolet image, the blue regions contain young massive stars."},
+					{"picture": "images/hoagsobject.png", 			"description": "images/names/hoagsobject.png", 			"long_description": "The Hoag's Object is an example of a ring galaxy. Ring galaxies are thought to occur when a smaller galaxy passes through a larger spiral galaxy."}, 
 				  ];
 
 	content = shuffle(content);
@@ -54,56 +56,60 @@ $(document).ready(function(){
 		$tmp_box = $(tmp_box);
 
 		// Make sure the box clicked is not the same box and that the box is not already hidden
-		if(first_clicked != sel.target.id && $tmp_box.css('opacity') != 0){
+		if(first_clicked != sel.target.id && $tmp_box.src != "images/question.png"){
 
-			var box_id = "#" + sel.target.id;
-			$(box_id).fadeTo(500, 0, function() {
+			// Show the image
+			$(tmp_box).attr('src', game_board[sel.target.id]);
 
-				// Fade in the hidden box
-				$(box_id).src = game_board[sel.target.id];
-				$(box_id).fadeTo(500, 1);
+			// Is this the first box or the second box?
+			if(clicked == 0){
 
-				// Is this the first box or the second box?
-				if(clicked == 0){
+				// Just let it sit until the next one is clicked
+				first_clicked = sel.target.id;
+				clicked = 1;
+				
+			}else{
 
-					// Just let it sit until the next one is clicked
-					first_clicked = sel.target.id;
-					clicked = 1;
-					
-				}else{
+				// Reset values and compare
+				second_clicked = sel.target.id;
+				clicked = 0;
+				
+				// Get the two clicked boxes
+				var box_id1 = "#" + first_clicked;
+				var box_id2 = "#" + second_clicked;
 
-					// Reset values and compare
-					second_clicked = sel.target.id;
-					clicked = 0;
+				// Check match
+				if(is_match(first_clicked, second_clicked)){
 
-					// Check match
-					if(is_match(first_clicked, second_clicked)){
+					// Remove from board
+					$(box_id1).css({opacity: 0});
+					$(box_id2).css({opacity: 0});
 
-						// Keep faded
+					// Update information panel for a more expanded look
+					$picture.attr('src', game_questions[match_id]["picture"]);
+					$description.attr('src', game_questions[match_id]["description"]);
+					$long_description.text(game_questions[match_id]["long_description"]);
 
-						// Update information panel for a more expanded look
-						$long_description.text("hi");
+					// Increment matches
+					matches++;
+					$match_counter.text(matches);
 
-						// Increment matches
-						matches++;
-						$match_counter.text(matches);
+					if(matches == 8){
 
-						if(matches == 8){
-
-							alert("You Win!\nRefresh the page for a new game\nwith new questions!");
-						}
-
-					}else{
-						
-						// Revert fade
-						var box_id1 = "#" + first_clicked;
-						var box_id2 = "#" + second_clicked;
-
-						$(box_id1).fadeTo(500, 1);
-						$(box_id2).fadeTo(500, 1);						
+						alert("You Win! Refresh the page for a new game with new questions!");
 					}
+
+				}else{
+					
+					// Wait 2 seconds then revert
+
+					setTimeout(function(){
+
+						$(box_id1).attr('src', "images/question.png");
+						$(box_id2).attr('src', "images/question.png");
+					},1500);
 				}
-			});
+			}
 		}
 	});
 
@@ -130,15 +136,21 @@ $(document).ready(function(){
 
 	function assign_boxes(){
 
-		// Pick first 8 elements to be on the grid (to mix up each game)
-		for(var i = 0; i < 8; i++){
+		// Gather 8 random elements for game_questions
+		game_questions = content;
+		game_questions = shuffle(game_questions);
 
-			game_questions[i] = content[i];
-			game_board[i] = content[i]["picture"];
-			game_board[i+8] = content[i]["description"];
+		// Add pictures to the board
+		for(var i = 0; i < 8; i++){
+			game_board[i] = game_questions[i]["picture"];
 		}
 
-		// Now we have 16 boxes and 16 pictures, we need to mix them up
+		// Add descriptions to the board
+		for(var i = 8; i < 16; i++){
+			game_board[i] = game_questions[i-8]["description"];
+		}
+
+		// Now we have 16 pictures, we need to mix them up
 		game_board = shuffle(game_board);
 	}
 
@@ -161,6 +173,7 @@ $(document).ready(function(){
 				if(game_questions[i]["description"] == content1 || 
 				   game_questions[i]["description"] == content2){
 
+					match_id = i;
 					matched = true;
 					break;
 				}
